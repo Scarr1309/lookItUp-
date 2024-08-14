f59e76f645a048d091cd5297d36cf994d6f062d7
Charan <saicharanallu1309@gmail.com>
2024-08-14 11:19:01 -0500

    Retrieve data from Google News

--- news_scrapping.py
+++ news_scrapping.py
@@ -1,10 +1,31 @@
-from requests_html import HTMLsession
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 
+# Create an HTML session
 mySession = HTMLSession()
 topStoriesUrl = "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen"
 
-renderPage = mySession.get(topStoriesUrl)
-renderPage.html.render(sleep = 1, scrowdown = 3)
+try:
+    # Fetch the page
+    renderPage = mySession.get(topStoriesUrl)
 
+    # Render the page with some sleep time and scroll down to ensure content loads
+    renderPage.html.render(sleep=1, scrolldown=1)  
 
+    articleTags = renderPage.html.find('article')
 
+    for tag in articleTags:
+      aTags = tag.find('a')
+      if(len(aTags) >= 2):
+        newsArticle = aTags[1]
+        newsTitle = newsArticle.text
+        titleLink = newsArticle.absolute_links
+        print(newsTitle, titleLink)
+      else:
+         continue
+        
+            
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    mySession.close()
